2af30a973a4593039d353b8e22249d928f6dc288
Scott Saltzman <salty923@gmail.com>
2017-12-26 21:18:56 -0600

    create html layout for topic buttons and gif display, create setup array for form push

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,4 +1,43 @@
 // alert("connected");
 
 
+
+var topics =["sports","fishing","music"];
+
+
 //// Api Key: AmOqTbPtuhUYCwI707dfOP5Lby5BLU0x
+
+$(document).ready(function () {
+
+    $("button").on("click", function () {
+  
+    var topic = $(this).attr("data-person");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        topic + "&api_key=AmOqTbPtuhUYCwI707dfOP5Lby5BLU0x";
+
+    // Performing our AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+            // Storing an array of results in the results variable
+            var results = response.data;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document ready closing
+});
